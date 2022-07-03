72609cbe48e33fae6a0ba0f3e6e45b2502ceaf41
ochufy <roshancm01@gmail.com>
2022-07-03 18:13:05 +0530

    add carousel

--- src/PopularCarousel.js
+++ src/PopularCarousel.js
@@ -1,10 +1,11 @@
 import MovieCard from "./MovieCard.js";
+import Carousel from 'react-material-ui-carousel'
 import Axios from "axios";
 import {useState} from "react";
 
 function PopularCarousel() {
 
-  var mostPopularMovies = "https://imdb-api.com/en/API/MostPopularMovies/k_w49t687e";
+  var mostPopularMovies = "https://imdb-api.com/en/API/MostPopularMovies/k_i9n94ek2";
   const [popularId1, setPopularId1] = useState("");
   const [popularId2, setPopularId2] = useState("");
   const [popularId3, setPopularId3] = useState("");
@@ -16,42 +17,26 @@ function PopularCarousel() {
     .then(
       (response) => {
         setPopularId1(response.data.items[0].id);
-      }
-  )
-  Axios.get(mostPopularMovies)
-    .then(
-      (response) => {
         setPopularId2(response.data.items[1].id);
-      }
-  )
-  Axios.get(mostPopularMovies)
-    .then(
-      (response) => {
         setPopularId3(response.data.items[2].id);
-      }
-  )
-  Axios.get(mostPopularMovies)
-    .then(
-      (response) => {
         setPopularId4(response.data.items[3].id);
-      }
-  )
-  Axios.get(mostPopularMovies)
-    .then(
-      (response) => {
         setPopularId5(response.data.items[4].id);
       }
   )
 
-
   return(
-    <>
+    <Carousel
+      autoPlay={false}
+      navButtonsAlwaysVisible
+      sx={{width:290}}
+    >
       <MovieCard imdbId={popularId1}/>
       <MovieCard imdbId={popularId2}/>
       <MovieCard imdbId={popularId3}/>
       <MovieCard imdbId={popularId4}/>
       <MovieCard imdbId={popularId5}/>
-    </>
+
+    </Carousel>
   )
 }
 
